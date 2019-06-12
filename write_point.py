de5cae71cdcc62b9c103775c29e19a3ed4e27489
马妙魁 <miaokui.mmk@alibaba-inc.com>
2019-06-12 15:17:33 +0800

    ok

--- write_point.py
+++ write_point.py
@@ -17,11 +17,11 @@ with open("/tmp/a.kpc", 'wb') as of:
     of.write(a)
     for i in range(0, len(arr)):
         index = i
-        # mod = i % 8
-        # if mod == 4 or mod == 5 :
-        #     index = i + 2
-        # if mod == 6 or mod == 7:
-        #     index = i - 2
+        mod = i % 8
+        if mod == 4 or mod == 5:
+            index = i + 2
+        if mod == 6 or mod == 7:
+            index = i - 2
         value = float(arr[index])/base
         print(value)
         a = struct.pack('f', value)
