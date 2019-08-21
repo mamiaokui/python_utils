231eba465b240250fe22d887675a650d03c1d8f4
马妙魁 <miaokui.mmk@alibaba-inc.com>
2019-08-21 10:14:38 +0800

    update

--- compress_video.py
+++ compress_video.py
@@ -1,6 +1,9 @@
 import sys
 import time
 import os
+import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+
 
 
 def list_all_files(rootdir):
@@ -19,27 +22,40 @@ def create_dirs(dir):
     if not os.path.exists(dir):
         os.makedirs(dir)
 
-def main():
+def process():
     input_dir = sys.argv[1]
     output_dir = sys.argv[2]
     files = list_all_files(input_dir)
 
     for file_name in files:
-        file_name= os.path.basename(file_name)
-        file_name = file_name.split(".")[0]
-        time_value = int(file_name.split("_")[1])
-        print(file_name)
-        timeArray = time.localtime(time_value)
-        file_name_full = time.strftime("%Y_%m_%d_%H_%M_%S", timeArray)
-        file_name_full += ".mp4"
-        file_name_hour = time.strftime("%Y%m%d%H", timeArray)
-        file_name_hour = os.path.join(output_dir, file_name_hour)
-        create_dirs(file_name_hour)
-        output_path = os.path.join(file_name_hour, file_name_full)
-        command = "ffmpeg -y -i %s -vf scale=320:-1 %s" %(file_name, output_path)
-        print(output_path)
+        try:
+            file_name_base= os.path.basename(file_name)
+            file_name_base = file_name_base.split(".")[0]
+            time_value = int(file_name_base.split("_")[1])
+            print(file_name_base)
+            timeArray = time.localtime(time_value)
+            file_name_full = time.strftime("%Y_%m_%d_%H_%M_%S", timeArray)
+            file_name_full += ".mp4"
+            file_name_hour = time.strftime("%Y%m%d%H", timeArray)
+            file_name_hour = os.path.join(output_dir, file_name_hour)
+            create_dirs(file_name_hour)
+            output_path = os.path.join(file_name_hour, file_name_full)
+            command = "ffmpeg -y -i %s -vf scale=320:-1 %s" %(file_name, output_path)
+            print(command)
+            ret = os.system(command)
+            if ret == 0:
+                os.remove(file_name)
+        except:
+            pass
 
+
+def schedule():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(process, 'interval', seconds=2)
+    scheduler.start()
 
 
 if __name__ == "__main__":
-    main()
+    schedule()
+    while True:
+        time.sleep(1000)
